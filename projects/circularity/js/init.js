8abca51c2c5e158bcda79dcfd9de65aef3bfdc6d
hunterschmillyvanilly <schmil111@stcharles.k12.la.us>
2023-04-25 18:36:41 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -76,11 +76,11 @@ var init = function (window) {
             if (circle.x < 0){
                 circle.x = canvas.width
             }
-            if (circle.y > 700){
+            if (circle.y > canvas.height){
                 circle.y = 0
             }
             if (circle.y < 0){
-                circle.y = 700
+                circle.y = canvas.height
             }
  
  
